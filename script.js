b6cafcb6366129cfc5a5e871ae5f59fb3ac3fc67
Oyindamola32 <oyindamolatolulope82@gmail.com>
2022-10-16 16:26:13 +0100

    modify script

--- script.js
+++ script.js
@@ -77,13 +77,16 @@ const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 console.log(amplitudeNew);
 */
 
+///////////////////////////////////////
+// Debugging with the Console and Breakpoints
 const measureKelvin = function () {
   const measurement = {
     type: 'temp',
     unit: 'celsius',
 
     // C) FIX
-    value: Number(prompt('Degree celsius:')),
+    //value: Number(prompt('Degree celsius:')),
+    value: 10,
   };
 
   // B) FIND
@@ -99,3 +102,26 @@ const measureKelvin = function () {
 
 // A) IDENTIFY
 console.log(measureKelvin());
+
+// Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    debugger;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// A) IDENTIFY
+console.log(amplitudeBug);
